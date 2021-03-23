7dcafa8550ae041766d74bfc15d1fa73c08c529c
krumilurfigur <gabrieloberg3@gmail.com>
2021-03-23 13:40:22 +0100

    added server and client

---
+++ src/clientModel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class clientModel {
+}
